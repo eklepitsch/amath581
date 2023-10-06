a937fb4aed2774492e86cd3633d07fd765229951
Evan Klepitsch <e.klepitsch@hotmail.com>
2023-10-05 20:47:51 -0500

    HW1, Problem 1, Part (b)

--- hw-1/hw1.py
+++ hw-1/hw1.py
@@ -24,7 +24,7 @@ def true_solution(t):
 
 
 # Part (a)
-def forward_euler(t0, tN, dt):
+def forward_euler(t0, tN, x0, dt):
     t = np.arange(t0, tN + dt / 2, dt)
     x = np.zeros_like(t)
     x[0] = x0
@@ -35,28 +35,61 @@ def forward_euler(t0, tN, dt):
     return t, x
 
 
+def heun(t0, tN, x0, dt):
+    t = np.arange(t0, tN + dt / 2, dt)
+    x = np.zeros_like(t)
+    x[0] = x0
+
+    for k in range(len(t) - 1):
+        x[k + 1] =\
+            x[k] + (dt / 2) * (f(x[k], t[k]) +
+                               f(x[k] + dt * f(x[k], t[k]), t[k] + dt))
+
+    return t, x
+
+
 # tplot = np.linspace(t0, tN, 10000)
 # xplot = true_solution(tplot)
 # plt.plot(tplot, xplot, 'k')
 
 dt = 2e-5
-t, approx_solution = forward_euler(t0, tN, dt)
+t, approx_solution = forward_euler(t0, tN, x0, dt)
 local_error = abs(true_solution(t[1]) - approx_solution[1])
 global_error = abs(true_solution(t[-1]) - approx_solution[-1])
 A1 = local_error
 A2 = global_error
 
 dt = 2e-6
-t, approx_solution = forward_euler(t0, tN, dt)
+t, approx_solution = forward_euler(t0, tN, x0, dt)
 local_error = abs(true_solution(t[1]) - approx_solution[1])
 global_error = abs(true_solution(t[-1]) - approx_solution[-1])
 A3 = local_error
 A4 = global_error
 
+print('Forward Euler:')
 print(f'A1 = {A1}')
 print(f'A2 = {A2}')
 print(f'A3 = {A3}')
 print(f'A4 = {A4}')
 
+dt = 2e-5
+t, approx_solution = heun(t0, tN, x0, dt)
+local_error = abs(true_solution(t[1]) - approx_solution[1])
+global_error = abs(true_solution(t[-1]) - approx_solution[-1])
+A5 = local_error
+A6 = global_error
+
+dt = 2e-6
+t, approx_solution = heun(t0, tN, x0, dt)
+local_error = abs(true_solution(t[1]) - approx_solution[1])
+global_error = abs(true_solution(t[-1]) - approx_solution[-1])
+A7 = local_error
+A8 = global_error
+
+print('Heun:')
+print(f'A5 = {A5}')
+print(f'A6 = {A6}')
+print(f'A7 = {A7}')
+print(f'A8 = {A8}')
 
 #plt.show()
